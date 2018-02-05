293db146483117a6518649b0299ff47d0ca0ab2d
yvzn <mail@ludeo.net>
2018-02-05 12:46:35 +0100

    More CRUD operations

--- WallboardBack/Controllers/WidgetsController.cs
+++ WallboardBack/Controllers/WidgetsController.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WallboardBack.Models;
 
@@ -28,6 +27,67 @@ namespace WallboardBack.Controllers
         public IEnumerable<Widget> GetAll()
         {
             return _context.Widgets.ToList();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Widget item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Widgets.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetWidget", new { id = item.Id }, item);
+        }
+
+        [HttpGet("{id}", Name = "GetWidget")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.Widgets.FirstOrDefault(t => t.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] Widget item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var widget = _context.Widgets.FirstOrDefault(t => t.Id == id);
+            if (widget == null)
+            {
+                return NotFound();
+            }
+
+            widget.Uri = item.Uri;
+            widget.Name = item.Name;
+
+            _context.Widgets.Update(widget);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var widget = _context.Widgets.FirstOrDefault(t => t.Id == id);
+            if (widget == null)
+            {
+                return NotFound();
+            }
+
+            _context.Widgets.Remove(widget);
+            _context.SaveChanges();
+            return new NoContentResult();
         }
     }
 }
